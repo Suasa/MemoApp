b2b0fc54dea628d2e440b74f5dd3a39a4f18d7b3
TsubasaTakagi <mizuirotech@gmail.com>
2019-12-04 12:04:49 +0900

    Style MemoDetailScreen

---
+++ src/screens/MemoDetailScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoDetailScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <View style={styles.memoHeader}>
+            <View>
+              <Text style={styles.memoHeaderTitle}>Idea of Lecture</Text>
+              <Text style={styles.memoHeaderDate}>2019/12/4</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.memoContent}>
+          <Text>This is an idea of lecture.</Text>
+        </View>
+
+        <CircleButton color="white" style={styles.editButton}>+</CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoHeader: {
+    padding: 10,
+    height: 100,
+    backgroundColor: '#17313C',
+    justifyContent: 'center',
+  },
+  memoHeaderTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 4,
+  },
+  memoHeaderDate: {
+    fontSize: 12,
+    color: '#fff',
+  },
+  memoContent: {
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    flex: 1,
+  },
+  editButton: {
+    top: 75,
+  },
+});
+
+export default MemoDetailScreen;
