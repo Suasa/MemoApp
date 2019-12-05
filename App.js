6072bd6b25ea7cea22e91e08c6e85583c720f5aa
TsubasaTakagi <mizuirotech@gmail.com>
2019-12-06 08:21:46 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import SignupScreen from './src/screens/SignupScreen';
+
+import ENV from './env.json';
+
+// eslint-disable-next-line
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_API_ID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
 const App = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
+  Signup: {
+    screen: SignupScreen,
+  },
   Home: {
     screen: MemoListScreen,
   },
@@ -17,16 +39,12 @@ const App = createStackNavigator({
   MemoEdit: {
     screen: MemoEditScreen,
   },
-  MemoLogin: {
-    screen: LoginScreen,
-  },
-  MemoSignup: {
-    screen: SignupScreen,
-  },
 }, {
-  initialRouteName: 'Home',
+  // initialRouteName: 'Home',
   defaultNavigationOptions: {
     headerTitle: 'Memot',
+    headerBackTitle: null,
+    headerTintColor: '#fff',
     headerStyle: {
       backgroundColor: '#265366',
     },
